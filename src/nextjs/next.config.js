cda12654413fbb4d7fc7b05058b2fb1d558dd1d8
UWC\Pisitpong.Moo <pisitpong.moo@dohome.co.th>
2022-12-01 13:40:57 +0700

    testliff

--- src/nextjs/next.config.js
+++ src/nextjs/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    //LIFF_ID: process.env.LIFF_ID,
-    LIFF_ID: "1657680615-z871Zkk4",
+    LIFF_ID: process.env.LIFF_ID,
+    //LIFF_ID: "1657680615-z871Zkk4",
   },
 };
